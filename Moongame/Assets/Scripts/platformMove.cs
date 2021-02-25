13aab6b40dd78ee52182d73aef38cc8c4b9ab12c
Brandon <wendellcasey1999@gmail.com>
2021-02-24 19:43:32 -0600

    Got a basic script for platform movement

---
+++ Moongame/Assets/Scripts/platformMove.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformMove : MonoBehaviour
+{
+
+    float dirX, moveSpeed = 3f;
+    private bool moving = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > 4f)
+            moving = false;
+        if (transform.position.x < -4f)
+            moving = true;
+
+        if (moving)
+            transform.position = new Vector2(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y);
+        else
+            transform.position = new Vector2(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y);
+    }
+}
